e2adb7dbd8d0db2e9ad0c1c4f6e2743daec836b5
jose <jose.silva2000@etec.sp.gov.br>
2022-11-04 22:42:08 -0300

    atividade

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Números pares entre 0 e ? ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+
+if (numeroDigitado >=1 )
+{
+   int numeropar = 0;
+
+    while (numeropar <= numeroDigitado)
+     {
+      Thread.Sleep(500);
+       Console.WriteLine($"{numeropar}"); 
+       numeropar += 2; 
+      
+
+    }
+                 
+}
+else
+{
+   Console.WriteLine($"o numero{numeroDigitado}não é possivel .");
+}
+
+Console.WriteLine("tchau  !!");
+Thread.Sleep(500);
+Console.WriteLine("e");
+Thread.Sleep(650);
+Console.WriteLine("obrigado !!");
+Thread.Sleep(500);
+
